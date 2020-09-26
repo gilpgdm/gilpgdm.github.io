14fab824fdb51b341c54e7316a254a88e4aa35f3
gilpgcl <gilpgcl@gmail.com>
2020-09-26 00:05:40 -0500

    Agrega versión inicial de PWA

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -8,8 +8,8 @@ customElements.define("mi-nav", class extends HTMLElement {
         <li><a href="1_app_multiplataforma.html">Las App Multiplataforma</a></li>
         <li><a href="2_html.html">Introducción a HTML5</a></li>
         <li><a href="4_css3.html">Introducción a CSS3</a></li>
-        <li><a href="3_multimedia.html">Elementos Multimedia</a></li>
         <li><a href="5_cajas.html">Modelo de Cajas</a></li>
+        <li><a href="3_multimedia.html">Elementos Multimedia</a></li>
         <li><a href="6_intro_js.html">Introducción a JavaScript</a></li>
         <li><a href="7_tipos.html">Tipos de Datos</a></li>
         <li><a href="8_formularios.html">Formularios</a></li>
@@ -20,6 +20,7 @@ customElements.define("mi-nav", class extends HTMLElement {
         <li><a href="13_objetos.html">Objetos</a></li>
         <li><a href="14_excepciones.html">Excepciones</a></li>
         <li><a href="15_dispositivo.html">Acceso al Dispositivo</a></li>
+        <li><a href="16_pwa.html">PWA</a></li>
       </ul>`;
   }
 });
