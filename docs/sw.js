66d12cda160f94e40f5d93d6417bdc27d8c29ade
gilpgdm <gilpgdm@gmail.com>
2022-03-22 00:22:11 -0600

    Corrige todos los ejemplos para ESP32

--- docs/sw.js
+++ docs/sw.js
@@ -239,9 +239,7 @@ const ARCHIVOS = [
    "/src/m05pwaMaterial/pwamd.zip",
    "/src/m05pwaMaterial/roboto-v29-latin-regular.woff",
    "/src/m05pwaMaterial/roboto-v29-latin-regular.woff2",
-   "/src/m07iot/composer.zip",
-   "/src/m08esp8266/Blink.ino",
-   "/src/m08esp8266/Button.ino" ]
+   "/src/m07iot/composer.zip" ]
 
 if (self instanceof ServiceWorkerGlobalScope) {
  self.addEventListener("install", installListener)
