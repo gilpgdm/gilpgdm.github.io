1859ef1f4721a8e162e857679fe24fe783e14595
gilpgdm <gilpgdm@gmail.com>
2024-01-04 14:28:35 -0600

    sw 5.16

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
 
-const VERSION = "5.15"
+const VERSION = "5.16"
 
 const CACHE = "cache"
 
